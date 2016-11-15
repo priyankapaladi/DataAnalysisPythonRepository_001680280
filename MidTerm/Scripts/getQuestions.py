e5c9c8296ab8c9495bb933a2c247436df6914ce2
Spandan Brahmbhatt <brahmbhattspandan@gmail.com>
2016-11-14 20:31:51 -0500

    Update getQuestions.py

--- MidTerm/Scripts/getQuestions.py
+++ MidTerm/Scripts/getQuestions.py
@@ -7,12 +7,14 @@ from time import sleep
 pageStr = '1'
 
 while True:
+    # Try not to hard code.
     url ='https://api.stackexchange.com/2.2/questions?page='+pageStr+'&pagesize=100&order=desc&sort=activity&site=stackoverflow&key=f49Nam3UjMJmLs2KHr1jcg(('
     response = requests.get(url)
     response = response.json()
     with open(pageStr+'.json', 'w') as f:
         json.dump(response, f)
     page = int(pageStr) + 1
+    # Seems unnecessary.
     pageStr = str(page)
     if page == 2500:
         break
